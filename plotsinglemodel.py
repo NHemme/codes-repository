3e65e14d05cd0a51019e8b05f815f3f7f68c0076
Nicoline Hemme <nicoline.krogh.hemme@cern.ch>
2021-11-29 20:12:59 +0100

    Commit before merging

---
+++ plotsinglemodel.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[201]:
+
+
+# contact: Suchita Kulkarni
+#suchita.kulkarni@cern.ch; suchita.kulkarni@gmail.com
+#!/usr/bin/env python
+import os, sys, shutil
+from numpy import *
+from pylab import *
+from scipy.interpolate import interp1d
+import ROOT
+#%run plotting_helpers.ipynb
+from plotting_helpers import *
+
+
+if len(sys.argv) < 2:
+    print("Usage: python /path/plotsinglemodel.py /path/output.root")
+    print("Optional arguments: histogram_number(integer) groomed_plots(boolean) scale_au(boolean) var_legend(boolean)")
+    sys.exit(1)
+    
+infile = sys.argv[1]
+if len(sys.argv) > 2:
+    histnumber = int(sys.argv[2])
+else:
+    histnumber = -1
+
+
+def MakeSinglePlot(infile = '', histnumber = int, groomed_plots = False, scale_au = True, var_legend = True):
+    rootfile1 = infile
+    histoname = GetAllHistoNames(infile)[0][histnumber]
+    histotitle = GetAllHistoNames(infile)[1][histnumber]
+    plot_title = "Plots/%s" %(histoname)
+    legend_positions = ['r','r','r','r','r','r','l','c','r','r','r','r','r','r','r','r','r','r']
+
+    ROOT.gROOT.SetBatch(True)
+    c=ROOT.TCanvas("c","c",800,600)
+    f0 = ROOT.TFile(rootfile1,"READ")
+    #f0.ls()
+    
+    # Get the histogram, you can rebin the histogram with the commented out rebin option
+    h0=f0.Get(histoname)
+    n_bins = h0.GetNbinsX()
+    divider = 25 #larger divider gives more bins
+    while n_bins%divider != 0 :
+        divider += 1
+    if divider == n_bins:
+        divider = divider/2
+    h0.Rebin(int(n_bins/divider))
+    #print(n_bins, int(n_bins/divider), h0.GetNbinsX())
+    # The following lines will normalise the histogram if scale_au=True and groomed_plots=False
+    if scale_au == True and groomed_plots == False:
+        norm = h0.GetEntries()
+        scale = 1/h0.Integral();
+        h0.Scale(scale);
+    
+    c.cd()
+    #Set axis title, you can use latex as demonstrated
+    h0.GetXaxis()
+    # Thie following lines will set the Y-axis title depending on whether it was normalised
+    if scale_au == True and groomed_plots == False:
+        h0.GetYaxis().SetTitle('A.U.')
+    else:
+        h0.GetYaxis()
+    #switch off the ugly stats box on the upper right corner
+    h0.SetStats(0)
+    
+    #set label sizes
+    h0.GetYaxis().SetLabelSize(0.04)
+    h0.GetYaxis().SetTitleSize(0.04)
+    h0.GetXaxis().SetTitleSize(0.04)
+    h0.GetXaxis().SetLabelSize(0.04)
+    h0.SetTitle("")
+    #set linewidth and color of the lines
+    h0.SetLineWidth(2)
+    h0.SetLineColor(ROOT.kBlue-2)
+    #actually draw the histogram
+    h0.Draw('hist')
+    h0.Draw('E1 same')
+    
+    #set axis offsets so that labels don't get messed up
+    h0.GetXaxis().SetTitleOffset(1.4)
+    h0.GetYaxis().SetTitleOffset(1.4)
+    #set axis ranges
+    max_x = h0.GetXaxis().GetXmax()
+    min_x = h0.GetXaxis().GetXmin()
+    h0.GetXaxis().SetRangeUser(min_x, max_x)
+    max_y = h0.GetBinContent(h0.GetMaximumBin())*1.1
+    h0.GetYaxis().SetRangeUser(0, max_y)
+    #set margins on canvas
+    c.SetRightMargin(0.09)
+    c.SetLeftMargin(0.15)
+    c.SetBottomMargin(0.15)
+
+    # Add a legend and write sample name, information etc. Position dependent on variable legend choice
+    latex = ROOT.TLatex ()
+    latex.SetNDC(ROOT.kTRUE)
+    if var_legend == True:
+        legendp = GetLegendPlacement(legend_positions[histnumber])
+    else: 
+        legendp = GetPosition('r')
+    a,b = legendp
+    latex.SetTextSize(0.04)
+    legend = ROOT.TLegend (a,0.64 ,b,0.74)
+    latex.DrawText(a+0.01, 0.85 , "Dark Shower Sample")
+    latex.SetTextSize(0.03)
+    latex.DrawLatex(a+0.01, 0.8 , "#bf{Higgs events at #sqrt{s}=13 TeV}")
+    latex.DrawLatex(a+0.01, 0.76 , "#bf{m_{H}=2 TeV, N_{c}=5, N_{f}=2}")
+    legend.AddEntry (histoname, histotitle)
+    legend.SetLineWidth (0)
+    legend.Draw("same")
+    
+    # Print statistics
+    stat_string = GetStatistics(h0,histotitle)
+    #print(stat_string)
+           
+    # Draws groomed histograms in same plot if groomed_plots=True and the groomed histograms exist
+    if groomed_plots == True:
+        try:
+            GetGroomedPlots(f0,c,histoname,h0.GetNbinsX(),legend)
+            plot_title = plot_title + "_groomed"
+        except ValueError:
+            print(sys.exc_info()[0], "occured: No groomed histograms exist of this kind. Will continue without groomed histograms.")
+            
+    # Save the canvas
+    plot_title = plot_title + ".png"
+    c.Print(plot_title)
+    
+
+if histnumber >= 0 and histnumber <= 17:
+    MakeSinglePlot(infile, histnumber, groomed_plots = False, scale_au = True, var_legend = True)
+else:
+    for hist in range(0,18):
+        if hist == 16:
+            continue
+        else:
+            MakeSinglePlot(infile, histnumber = hist, groomed_plots = False, scale_au = True, var_legend = True)
---
+++ plotting_helpers.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import numpy as np
+import ROOT
+
+def GetAllHistoNames(infile = ''):
+    rootfile1 = infile
+    f0 = ROOT.TFile(rootfile1,"READ")
+    
+    listofnames = []
+    listoftitles = []
+    
+    for obj in f0.GetListOfKeys():
+        listofnames.append(obj.GetName())
+        listoftitles.append(obj.GetTitle())
+    
+    return listofnames, listoftitles
+
+
+def GetLegendPlacement(position = ''):
+        if position == 'r':
+            placement = [0.62,0.9]
+        if position == 'c':
+            placement = [0.4,0.67]
+        if position == 'l':
+            placement = [0.19,0.48]
+        return placement
+
+def GetStatistics(histogram,histotitle):
+    h0 = histogram
+    p = np.array([0.25,0.5,0.75])
+    q = np.array([0.0,0.0,0.0])
+    h0.GetQuantiles(3,q,p)
+    x_unit = h0.GetXaxis().GetTitle()
+    if x_unit.find("trk") != -1:
+        x_unit = x_unit
+    else:
+        index = x_unit.find("[")
+        x_unit = x_unit[index+1:-1]
+    stat_string = """Some statistics for %s: 
+    There are %i events in the histogram. They are binned into %i bins. 
+    The mean is %0.2f %s and the standard deviation is %0.2f %s. 
+    Half of the events are contained between %0.2f and %0.2f %s.
+    """%(histotitle,h0.GetEntries(), h0.GetNbinsX(),h0.GetMean(),x_unit,h0.GetStdDev(),x_unit,q[1],q[2],x_unit)
+    return stat_string
+
+
+    
+def GetGroomedPlots(rootfile, canvas, histoname, n_bins,legend):
+    h1_name = histoname + "_softdrop"
+    h2_name = histoname + "_trimmed"
+    # The following line checks if the groomed histograms exist and will raise an error and leave the function if not
+    if rootfile.GetListOfKeys().Contains(h1_name) == False or rootfile.GetListOfKeys().Contains(h2_name) == False:
+        raise ValueError
+        return
+    # Enables drawing on the same canvas
+    canvas.cd()
+    h1=rootfile.Get(h1_name)
+    h2=rootfile.Get(h2_name)
+    # Binning depends on the n_bins of the un-groomed histogram so that they have equal number of bins
+    h1_bins = h1.GetNbinsX()
+    binning = h1_bins/n_bins
+    h1.Rebin(int(binning))
+    h2_bins = h2.GetNbinsX()
+    binning = h2_bins/n_bins
+    h2.Rebin(int(binning))
+    # Sets options for drawing and draws
+    h1.SetLineWidth(2)
+    h1.SetLineColor(ROOT.kMagenta-3)
+    h1.DrawCopy('hist same')
+    h1.DrawCopy('E1 same')
+    h2.SetLineWidth(2)
+    h2.SetLineColor(ROOT.kCyan-3)
+    h2.DrawCopy('hist same')
+    h2.DrawCopy('E1 same')
+    # Defined titles and adds legends
+    h1_title = h1.GetTitle()
+    index = h1_title.find('with')
+    h1_title = h1_title[:index] + "soft dropped"
+    h2_title = h2.GetTitle()
+    index = h2_title.find('with')
+    h2_title = h2_title[:index] + "trimmed"
+    legend.AddEntry(h1,h1_title)
+    legend.AddEntry(h2,h2_title)
+    print(GetStatistics(h1,h1_name))
+    print(GetStatistics(h2,h2_name))
+    
+    
+
